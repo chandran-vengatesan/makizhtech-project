5ec285a2cec612437b60389cdc695131ee47b343
chandran <chandranpv92@gmail.com>
2026-02-18 23:30:53 +0530

    Update products.jsx

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -4,34 +4,35 @@ export const products = [
     name: "Temperature Sensor",
     description: "High precision sensor",
     price: 499,
-    image: "public/images/Arduino-Temperature-Sensor.png"
+    image: "public/Images/Arduino-Temperature-Sensor.png"
   },
   {
     id: 2,
     name: "Motion Sensor",
     description: "Smart motion detection",
     price: 799,
-    image: "public/images/motion_sensor.png"
+    image: "public/Images/motion_sensor.png"
   },
   {
     id: 3,
     name: "Gas Sensor",
     description: "Industrial safety sensor",
     price: 999,
-    image: "public/images/gas_sensor.png"
+    image: "public/Images/gas_sensor.png"
   },
   {
     id: 4,
     name: "Humidity Sensor",
     description: "Accurate environment tracking",
     price: 599,
-    image: "public/images/humidity_sensor.png"
+    image: "public/Images/humidity_sensor.png"
   },
   {
     id: 5,
     name: "IR Sensor",
     description: "Infrared detection module",
     price: 399,
-    image: "public/images/ir_sensor.png"
+    image: "public/Images/ir_sensor.png"
   }
 ];
+
